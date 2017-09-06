5b30a900535e26543e0f5a42420010fc2a51edf2
Pavel Rubin <pash7ka@gmail.com>
2017-09-06 23:39:38 +0300

    First BurnableToken implementation

---
+++ contracts/BurnableToken.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.4.12;
+
+import './zeppelin/math/SafeMath.sol';
+import './zeppelin/token/StandardToken.sol';
+
+
+contract BurnableToken is StandardToken {
+    using SafeMath for uint256;
+
+    event Burn(address indexed from, uint256 amount);
+    event BurnRewardIncreased(address indexed from, uint256 value);
+
+    /**
+    * @dev Sending ether to contract increases burning reward 
+    */
+    function() payable {
+        if(msg.value > 0){
+            BurnRewardIncreased(msg.sender, msg.value);    
+        }
+    }
+
+    /**
+     * @dev Calculates how much ether one will receive in reward for burning tokens
+     * @param _amount of tokens to be burned
+     */
+    function burnReward(uint256 _amount) public constant returns(uint256){
+        return this.balance.mul(_amount).div(totalSupply);
+    }
+
+    /**
+    * @dev Burns tokens and send reward
+    * This is internal function because it DOES NOT check 
+    * if _from has allowance to burn tokens.
+    * It is intended to be used in transfer() and transferFrom() which do this check.
+    * @param _from The address which you want to burn tokens from
+    * @param _amount of tokens to be burned
+    */
+    function burn(address _from, uint256 _amount) internal returns(bool){
+        require(balances[_from] >= _amount);
+        
+        uint256 reward = burnReward(_amount);
+        assert(this.balance - reward > 0);
+
+        balances[_from] = balances[_from].sub(_amount);
+        totalSupply = totalSupply.sub(_amount);
+        //assert(totalSupply >= 0); //Check is not needed because totalSupply.sub(value) will already throw if this condition is not met
+        
+        _from.transfer(reward);
+        Burn(_from, _amount);
+        return true;
+    }
+
+    /**
+    * @dev Transfers or burns tokens
+    * Burns tokens transferred to this contract itself or to zero address
+    * @param _to The address to transfer to or token contract address to burn.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) returns (bool) {
+        if( (_to == address(this)) || (_to == 0) ){
+            return burn(msg.sender, _value);
+        }else{
+            return super.transfer(_to, _value);
+        }
+    }
+
+    /**
+    * @dev Transfer tokens from one address to another 
+    * or burns them if _to is this contract or zero address
+    * @param _from address The address which you want to send tokens from
+    * @param _to address The address which you want to transfer to
+    * @param _value uint256 the amout of tokens to be transfered
+    */
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+        if( (_to == address(this)) || (_to == 0) ){
+            var _allowance = allowed[_from][msg.sender];
+            //require (_value <= _allowance); //Check is not needed because _allowance.sub(_value) will already throw if this condition is not met
+            allowed[_from][msg.sender] = _allowance.sub(_value);
+            return burn(_from, _value);
+        }else{
+            return super.transferFrom(_from, _to, _value);
+        }
+    }
+
+}
+
+
